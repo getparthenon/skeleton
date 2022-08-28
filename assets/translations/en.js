4f1371f70a58f43441760b22ead70a6a720cba9c
Iain Cambridge <iain@humblyarrogant.io>
2022-08-28 11:57:12 +0200

    Add global i18n file

--- assets/translations/en.js
+++ assets/translations/en.js
@@ -1,5 +1,7 @@
 import {LOGIN_TRANSLATIONS} from "./en/login";
+import {GLOBAL_TRANSLATIONS} from "./en/global";
 
 export const ENGLISH_TRANSLATIONS = {
-    login: LOGIN_TRANSLATIONS
+    global: GLOBAL_TRANSLATIONS,
+    login: LOGIN_TRANSLATIONS,
 }
