ce3bea0db724896dfae8e43f84e8f274f771ca23
Iain Cambridge <iain@humblyarrogant.io>
2022-08-28 18:17:47 +0200

    Login page style start

--- assets/components/Login.vue
+++ assets/components/Login.vue
@@ -1,7 +1,21 @@
 <template>
-  <div class="flex items-center justify-center h-screen">
-    <div class="p-5">
-      {{ $t('login.title') }}
+  <div class="flex items-center justify-center h-screen login">
+    <div class="p-5 form-body">
+      <div class="w-full">
+        <img src="/images/logo.svg" alt="" class="m-auto" width="450" />
+      </div>
+      <h1 class="h1 text-center">{{ $t('login.title') }}</h1>
+      <div class="px-5 mb-3">
+        <label class="block mb-1">{{ $t('login.email') }}</label>
+        <input type="text" class="field" />
+      </div>
+      <div class="px-5 mb-3">
+        <label class="block mb-1">{{ $t('login.password') }}</label>
+        <input type="password" class="field" />
+      </div>
+      <div class="px-5">
+        <button class="btn--main w-full">{{ $t('login.login_button') }}</button>
+      </div>
     </div>
   </div>
 </template>
@@ -13,5 +27,23 @@ export default {
 </script>
 
 <style scoped>
+.field {
+  @apply bg-transparent rounded-lg text-slate-600 font-medium w-full backdrop-blur-lg;
+}
 
+.login {
+  background-color: #368aca;
+  background-image: linear-gradient(160deg, #368aca 0%, #c2dcef 100%);
+}
+
+.form-body {
+  @apply rounded-xl w-full md:max-w-screen-sm;
+  background: rgba(255, 255, 255, 0.2);
+  box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+  backdrop-filter: blur(5px);
+  -webkit-backdrop-filter: blur(5px);
+  border: 1px solid rgba(255, 255, 255, 0.3);
+
+  animation: fadeIn 1s;
+}
 </style>
