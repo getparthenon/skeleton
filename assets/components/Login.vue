653f8ffecafb71a8b3d1df085b7b40273a651572
Iain Cambridge <iain@humblyarrogant.io>
2022-08-30 12:13:37 +0200

    Add remember me

--- assets/components/Login.vue
+++ assets/components/Login.vue
@@ -13,6 +13,17 @@
         <label class="block mb-1">{{ $t('login.password') }}</label>
         <input type="password" class="field" />
       </div>
+      <div class="px-5 mb-3 flex items-center justify-between">
+        <div class="flex items-start">
+          <div class="flex items-center h-5">
+            <input id="remember" aria-describedby="remember" type="checkbox" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800" required="">
+          </div>
+          <div class="ml-3 text-sm">
+            <label for="remember" class="text-black dark:text-white">Remember me</label>
+          </div>
+        </div>
+        <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</a>
+      </div>
       <div class="px-5">
         <button class="btn--main w-full">{{ $t('login.login_button') }}</button>
       </div>
