fc93a32fd49e44a98082f3caee859e3d257ad50d
Iain Cambridge <iain@humblyarrogant.io>
2022-09-08 11:45:47 +0200

    Change license

--- assets/translations/en/app/team/main.js
+++ assets/translations/en/app/team/main.js
@@ -1,3 +1,4 @@
 export const TEAM_MAIN_TRANSLATIONS = {
         title: "Main",
+        add_team_member: "Add Team Member",
 }
--- assets/views/App/TeamSettings.vue
+++ assets/views/App/TeamSettings.vue
@@ -2,6 +2,10 @@
   <div>
     <h1 class="page-title">{{ $t('app.team.main.title') }}</h1>
 
+    <div class="top-button-container">
+      <button class="btn--main"><i class="fa-solid fa-user-plus"></i> {{ $t('app.team.main.add_team_member') }}</button>
+    </div>
+
     <div class="card-body">
       Test
     </div>
