83052faac149c3704689e33950652713d5f1cef7
Iain Cambridge <iain@humblyarrogant.io>
2022-09-12 19:45:06 +0200

    Add in progress to cancel button

--- assets/views/App/Plan.vue
+++ assets/views/App/Plan.vue
@@ -51,7 +51,14 @@
       <div class="cancle_bttn mt-3" v-if="current_plan.plan_name === planName && (current_plan.plan_name == planName) && (current_plan.status == 'active' || current_plan.status == 'pending')">
         <a href="/api/payments/portal" class="btn--main mb-3 text-center block">{{ $t('app.plan.main.payment_settings') }}</a>
 
-        <a @click="cancel" class="btn--danger block text-center cursor-pointer	">{{ $t('app.plan.main.cancel_button') }}</a>
+        <a @click="cancel" class="btn--danger block text-center cursor-pointer	"  v-if="in_progress === false">{{ $t('app.plan.main.cancel_button') }}</a>
+        <a @click="cancel" class="btn--danger--disabled block text-center "  v-else>
+          <svg class="animate-spin -ml-1 mr-3 h-5 w-5 text-white inline" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+          </svg>
+          {{ $t('app.plan.main.in_progress') }}
+        </a>
       </div>
     </div>
     </div>
@@ -65,9 +72,6 @@ import { stripeservice } from "../../services/stripeservice";
 
 export default {
   name: "Plan.vue",
-  components: {
-    StripeCheckout,
-  },
   data() {
     return {
       loading: false,
@@ -108,12 +112,14 @@ export default {
       )
     },
     cancel: function () {
+      this.in_progress = true;
       planservice.cancel().then(
           response => {
-            
+            this.in_progress = false;
           },
           error => {
             this.error_message = error;
+            this.in_progress = false;
           }
       )
     }
