38a3e1146cd25664459b1a65e27e25e657f928bc
Iain Cambridge <iain@humblyarrogant.io>
2022-09-22 10:08:36 +0200

    [Skeleton] Update starting migration

--- migrations/Version20220922080749.php
+++ migrations/Version20220922080749.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220830182726 extends AbstractMigration
+final class Version20220922080749 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,8 +20,6 @@ final class Version20220830182726 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE SEQUENCE parthenon_rule_engine_rule_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
-        $this->addSql('CREATE SEQUENCE parthenon_rule_engine_rule_execution_log_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
         $this->addSql('CREATE TABLE forgot_password_code (id UUID NOT NULL, user_id UUID DEFAULT NULL, code VARCHAR(255) NOT NULL, used BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, expires_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, used_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_B30A7571A76ED395 ON forgot_password_code (user_id)');
         $this->addSql('COMMENT ON COLUMN forgot_password_code.id IS \'(DC2Type:uuid)\'');
@@ -42,8 +40,6 @@ final class Version20220830182726 extends AbstractMigration
         $this->addSql('CREATE TABLE parthenon_ab_experiments (id UUID NOT NULL, type VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, desired_result VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, activated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, is_deleted BOOLEAN NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE UNIQUE INDEX UNIQ_6C53D6955E237E06 ON parthenon_ab_experiments (name)');
         $this->addSql('COMMENT ON COLUMN parthenon_ab_experiments.id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE parthenon_rule_engine_rule (id INT NOT NULL, entity VARCHAR(255) NOT NULL, action VARCHAR(255) NOT NULL, comparison VARCHAR(255) NOT NULL, field VARCHAR(255) NOT NULL, value VARCHAR(255) NOT NULL, options JSON DEFAULT NULL, PRIMARY KEY(id))');
-        $this->addSql('CREATE TABLE parthenon_rule_engine_rule_execution_log (id INT NOT NULL, entity_name VARCHAR(255) NOT NULL, field_name VARCHAR(255) NOT NULL, entity_id VARCHAR(255) NOT NULL, value JSON NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE TABLE team_invite_codes (id UUID NOT NULL, user_id UUID DEFAULT NULL, invited_user_id UUID DEFAULT NULL, team_id UUID DEFAULT NULL, code VARCHAR(255) NOT NULL, email VARCHAR(255) NOT NULL, used BOOLEAN NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, used_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, cancelled BOOLEAN NOT NULL, role VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_E05E9270A76ED395 ON team_invite_codes (user_id)');
         $this->addSql('CREATE INDEX IDX_E05E9270C58DAD6E ON team_invite_codes (invited_user_id)');
@@ -52,7 +48,7 @@ final class Version20220830182726 extends AbstractMigration
         $this->addSql('COMMENT ON COLUMN team_invite_codes.user_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN team_invite_codes.invited_user_id IS \'(DC2Type:uuid)\'');
         $this->addSql('COMMENT ON COLUMN team_invite_codes.team_id IS \'(DC2Type:uuid)\'');
-        $this->addSql('CREATE TABLE teams (id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, subscription_price_id VARCHAR(255) DEFAULT NULL, subscription_plan_name VARCHAR(255) DEFAULT NULL, subscription_payment_schedule VARCHAR(255) DEFAULT NULL, subscription_active BOOLEAN DEFAULT NULL, subscription_status VARCHAR(255) DEFAULT NULL, subscription_started_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_valid_until TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_ended_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_payment_id VARCHAR(255) DEFAULT NULL, subscription_checkout_id VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id))');
+        $this->addSql('CREATE TABLE teams (id UUID NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, name VARCHAR(255) DEFAULT NULL, subscription_price_id VARCHAR(255) DEFAULT NULL, subscription_plan_name VARCHAR(255) DEFAULT NULL, subscription_payment_schedule VARCHAR(255) DEFAULT NULL, subscription_active BOOLEAN DEFAULT NULL, subscription_status VARCHAR(255) DEFAULT NULL, subscription_started_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_valid_until TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_updated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_ended_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, subscription_payment_id VARCHAR(255) DEFAULT NULL, subscription_checkout_id VARCHAR(255) DEFAULT NULL, subscription_seats INT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('COMMENT ON COLUMN teams.id IS \'(DC2Type:uuid)\'');
         $this->addSql('CREATE TABLE users (id UUID NOT NULL, team_id UUID DEFAULT NULL, email VARCHAR(255) NOT NULL, password VARCHAR(255) NOT NULL, name VARCHAR(255) DEFAULT NULL, confirmation_code VARCHAR(255) NOT NULL, created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL, activated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deactivated_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, deleted_at TIMESTAMP(0) WITHOUT TIME ZONE DEFAULT NULL, is_confirmed BOOLEAN NOT NULL, is_deleted BOOLEAN NOT NULL, roles TEXT DEFAULT NULL, PRIMARY KEY(id))');
         $this->addSql('CREATE INDEX IDX_1483A5E9296CD8AE ON users (team_id)');
@@ -73,8 +69,6 @@ final class Version20220830182726 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('DROP SEQUENCE parthenon_rule_engine_rule_id_seq CASCADE');
-        $this->addSql('DROP SEQUENCE parthenon_rule_engine_rule_execution_log_id_seq CASCADE');
         $this->addSql('ALTER TABLE forgot_password_code DROP CONSTRAINT FK_B30A7571A76ED395');
         $this->addSql('ALTER TABLE invite_codes DROP CONSTRAINT FK_E8D89FB2A76ED395');
         $this->addSql('ALTER TABLE invite_codes DROP CONSTRAINT FK_E8D89FB2C58DAD6E');
@@ -88,8 +82,6 @@ final class Version20220830182726 extends AbstractMigration
         $this->addSql('DROP TABLE notification');
         $this->addSql('DROP TABLE parthenon_ab_experiment_variant');
         $this->addSql('DROP TABLE parthenon_ab_experiments');
-        $this->addSql('DROP TABLE parthenon_rule_engine_rule');
-        $this->addSql('DROP TABLE parthenon_rule_engine_rule_execution_log');
         $this->addSql('DROP TABLE team_invite_codes');
         $this->addSql('DROP TABLE teams');
         $this->addSql('DROP TABLE users');
