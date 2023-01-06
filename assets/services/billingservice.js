a2d8f87eb26e04efd6ecd5e98d26ef5cb06fa6f7
Iain Cambridge <iain@humblyarrogant.io>
2023-01-06 09:49:44 +0000

    [Billing] rename endpoint from billing/address to billing/details
    Automatic commit for https://github.com/getparthenon/monorepo/commit/dd522ffa144b3df0fa22c4d8e31ecaf63f33d4f7

--- assets/services/billingservice.js
+++ assets/services/billingservice.js
@@ -2,14 +2,14 @@ import axios from "axios";
 import {handleResponse} from "./utils";
 
 function sendAddress(address) {
-    return axios.post("/api/billing/address", address).then(handleResponse);
+    return axios.post("/api/billing/details", address).then(handleResponse);
 }
 function saveToken(token) {
     return axios.post("/api/billing/card/token/add", {token}).then(handleResponse);
 }
 
 function getAddress() {
-    return axios.get("/api/billing/address").then(handleResponse);
+    return axios.get("/api/billing/details").then(handleResponse);
 }
 
 function getAddCardToken() {
