3981cbbae20e12320412d0afc86e2c995a0601fa
Iain <iain@humblyarrogant.io>
2024-06-11 14:28:45 +0000

    [Billing] Make invoices only supported via BillaBear
    Automatic commit for https://github.com/getparthenon/monorepo/commit/e14a8f0c7e52780be28fcc00da9f76068d375528

--- assets/views/App/Billing/BillingInvoices.vue
+++ assets/views/App/Billing/BillingInvoices.vue
@@ -2,35 +2,43 @@
   <div>
     <h2 class="text-2xl mb-4">{{ $t('app.billing.invoices.title') }}</h2>
 
-    <div class="alert-error" v-if="showError">
-      {{ $t('app.billing.invoices.error_message') }}
-    </div>
+    <LoadingScreen :ready="ready">
+      <div v-if="supported">
 
-    <table class="table table-striped">
-      <thead>
-        <tr>
-          <th>{{ $t('app.billing.invoices.list.amount_due') }}</th>
-          <th>{{ $t('app.billing.invoices.list.created_at') }}</th>
-          <th>{{ $t('app.billing.invoices.list.status') }}</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="invoice in invoices">
-          <td>{{ displayCurrency(invoice.amount) }} {{ invoice.currency }}</td>
-          <td>{{ displayTime(invoice.created_at) }}</td>
-          <td>
-            <span class="badge--green" v-if="invoice.paid">{{ $t('app.billing.invoices.list.paid') }}</span>
-            <span class="badge--red"  v-else>{{ $t('app.billing.invoices.list.unpaid') }}</span>
-          </td>
-          <td>
-            <SubmitButton :in-progress="downloadInProgress" @click="download(invoice.id)" v-if="invoice.paid">{{ $t('app.billing.invoices.list.download') }}</SubmitButton>
-            <SubmitButton :in-progress="downloadInProgress" @click="charge(invoice)" v-else>{{ $t('app.billing.invoices.list.charge') }}</SubmitButton>
+        <div class="alert-error" v-if="showError">
+          {{ $t('app.billing.invoices.error_message') }}
+        </div>
 
-          </td>
-        </tr>
-      </tbody>
-    </table>
+        <table class="table table-striped">
+          <thead>
+          <tr>
+            <th>{{ $t('app.billing.invoices.list.amount_due') }}</th>
+            <th>{{ $t('app.billing.invoices.list.created_at') }}</th>
+            <th>{{ $t('app.billing.invoices.list.status') }}</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr v-for="invoice in invoices">
+            <td>{{ displayCurrency(invoice.amount) }} {{ invoice.currency }}</td>
+            <td>{{ displayTime(invoice.created_at) }}</td>
+            <td>
+              <span class="badge--green" v-if="invoice.paid">{{ $t('app.billing.invoices.list.paid') }}</span>
+              <span class="badge--red"  v-else>{{ $t('app.billing.invoices.list.unpaid') }}</span>
+            </td>
+            <td>
+              <SubmitButton :in-progress="downloadInProgress" @click="download(invoice.id)" v-if="invoice.paid">{{ $t('app.billing.invoices.list.download') }}</SubmitButton>
+              <SubmitButton :in-progress="downloadInProgress" @click="charge(invoice)" v-else>{{ $t('app.billing.invoices.list.charge') }}</SubmitButton>
+
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </div>
+      <div v-else>
+        This feature is currently only supported if you're using <a href="https://billabear.com/?utm_source=parthenon&utm_medium=inapp" target="_blank">BillaBear</a>.
+      </div>
+    </LoadingScreen>
   </div>
 
 </template>
@@ -44,6 +52,8 @@ export default {
   name: "BillingInvoices",
   data() {
     return {
+      ready: false,
+      supported: true,
       invoices: [],
       downloadInProgress: false,
       showError: false,
@@ -51,7 +61,9 @@ export default {
   },
   mounted() {
     axios.get("/api/billing/invoices").then((response) => {
+      this.ready = true;
       this.invoices = response.data.invoices;
+      this.supported = response.data.supported;
     })
   },
   methods: {
